bf3ad5835bcee9d03e7874f357fe8cdae78b0b55
brook <grini.abdellah@gmail.com>
2017-04-13 17:59:12 +0100

    log

--- src/Brains/UserBundle/Entity/User.php
+++ src/Brains/UserBundle/Entity/User.php
@@ -23,5 +23,70 @@ class User extends BaseUser
    * @ORM\GeneratedValue(strategy="AUTO")
    */
   protected $id;
+
+
+  /**
+     * @var string
+     *
+     * @ORM\Column(name="annee", type="string", length=500)
+     */
+    private $annee;
+
+      /**
+     * @var string
+     *
+     * @ORM\Column(name="filiere", type="string", length=500)
+     */
+    private $filiere;
+
+ /**
+     * Get annee
+     *
+     * @return string
+     */
+    public function getAnnee()
+    {
+        return $this->annee;
+    }
+
+  /**
+     * Set year
+     *
+     * @param string $annee
+     *
+     * @return Book
+     */
+    public function setAnnee($annee)
+    {
+        $this->annee = $annee;
+
+        return $this;
+    }
+
+     /**
+     * Get filiere
+     *
+     * @return string
+     */
+    public function getFiliere()
+    {
+        return $this->filiere;
+    }
+
+  /**
+     * Set filiere
+     *
+     * @param string $annee
+     *
+     * @return Book
+     */
+    public function setFiliere($filiere)
+    {
+        $this->filiere = $filiere;
+
+        return $this;
+    }
+
+
 }
 
