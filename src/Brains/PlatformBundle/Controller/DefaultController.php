28bc3d9f623502521db4f805b773aa7362f9b8bd
brook <grini.abdellah@gmail.com>
2017-04-19 14:47:51 +0100

    f

--- src/Brains/PlatformBundle/Controller/DefaultController.php
+++ src/Brains/PlatformBundle/Controller/DefaultController.php
@@ -18,10 +18,10 @@ class DefaultController extends Controller
     {
     	for($i=0;$i<2;$i++){
     	 $message = \Swift_Message::newInstance()
-        ->setSubject('Hello Email')
+        ->setSubject('Hello Email'.($i+1))
         ->setFrom('test.rmay.no2@gmail.com')
-        ->setTo('grini.abdellah@gmail.com')
-        ->setBody("hello world ".($i+1));
+        ->setTo('abdellah.grini@kindyinfomaroc.com')
+        ->setBody("watta lah yhdik !!");
         $this->get('mailer')->send($message);
 }
 
--- src/Brains/PlatformBundle/Controller/ShowController.php
+++ src/Brains/PlatformBundle/Controller/ShowController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 //use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -40,26 +45,57 @@ class ShowController extends Controller
   }
 
 
-/*
+
        public function update_filiereAction(Request $request, $id)
     {
+      
 $repository = $this  ->getDoctrine()  ->getManager()  ->getRepository('BrainsPlatformBundle:Filiere');
 
-$filiere = $repository->find($id)();
+$filiere = $repository->find($id);
+
  
 
 if (null === $filiere) {
       throw new NotFoundHttpException("Votre filière na pas été trouvée");
     }
 
+
+
+$formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $filiere);
+
+$form=$formBuilder
+      ->add('short',   TextType::class)
+      ->add('nome',   TextType::class)
+      ->add('Sauvegarder',      SubmitType::class)
+      ->getForm()      ;
+
+if($request->isMethod('POST')){
+
+    $form->handleRequest($request);
+
+    if($form->isValid()){
+        $em= $this->getDoctrine()->getManager();
+        $em->persist($filiere);
+        $em->flush();
+
+        $request->getSession()->getFlashBag()->add('notice', 'Filière Bien enregistrée.');
+
+        return $this->redirectToRoute('BP_show_filiere');
+    }
+}
+
+return $this->render('BrainsPlatformBundle:New:year.html.twig', array(
+ 'form'=>$form->createView(),
+  ));
+/*
 return $this->render('BrainsPlatformBundle:Show:filiere.html.twig', array(
       'filiere' => $filiere
-    ) );
+    ) );*/
 
 
 
 
-    }*/
+    }
 
 
 
