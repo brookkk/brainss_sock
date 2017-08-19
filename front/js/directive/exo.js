59db164d3ffb9f1f866965dfd7aa1690616425ba
brookkk <grini.abdellah@gmail.com>
2017-08-19 23:22:14 +0100

    directives

--- front/js/directive/exo.js
+++ front/js/directive/exo.js
@@ -18,6 +18,10 @@ app.directive('time', function(dateFilter, $interval){
         link: function(scope, element, attrs){
             scope.time = dateFilter(new Date(), 'hh:mm:ss');
 
+            element.on('$destroy', function(){
+                $interval.cancel(interval);
+            })
+
             interval = $interval(function(){
                 scope.time = dateFilter(new Date(), 'hh:mm:ss');
                 console.log('time changed');
