312239815e36ca42474b9a856589b4e7e382e015
brookkk <grini.abdellah@gmail.com>
2017-08-19 22:43:07 +0100

    directives

--- front/js/directive/exo.js
+++ front/js/directive/exo.js
@@ -1,15 +1,6 @@
 app.directive('ngExo', function(){
     return {
-        template :"	<h1>{{exercice.nom}}</h1>
-        Par : {{exercice.auteur}}<br>
-
-
-    <p>
-    <br><br><br>
-    exo créé le  : {{exercice.date_creation}}<br>
-    lien de l'exo  : {{exercice.link}}<br>
-
-    </p>"
+        template :"	"
         
 };
 })
