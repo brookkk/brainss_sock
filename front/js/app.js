c280de64727d5934407bf2f769d47e63440a8bbf
brookkk <grini.abdellah@gmail.com>
2017-08-19 23:57:06 +0100

    directives

--- front/js/app.js
+++ front/js/app.js
@@ -2,7 +2,7 @@
     app.config(function($routeProvider){
         $routeProvider
             .when('/', {templateUrl: 'partials/filieres.html', controller: 'FilieresCtrl'})
-            .when('/comments/:id', {templateUrl: 'partials/comments.html', controller: 'CommentsCtrl'})
+            //.when('/comments/:id', {templateUrl: 'partials/comments.html', controller: 'CommentsCtrl'})
             .when('/exercices/:id', {templateUrl: 'partials/exercices.html', controller: 'ExercicesCtrl'})
             .when('/exercices/:id/contenu/:id_exo', {templateUrl: 'partials/contenu.html', controller: 'ExercicesContentCtrl'})
             .otherwise({redirectTo: '/'});
--- front/js/directive/directive.js
+++ front/js/directive/directive.js
@@ -16,7 +16,7 @@ app.directive('time', function(dateFilter, $interval){
 
         },
         link: function(scope, element, attrs){
-            scope.time = dateFilter(new Date(), 'hh:mm:ss');
+            scope.time = dateFilter(new Date(80), 'hh:mm:ss');
 
             element.on('$destroy', function(){
                 $interval.cancel(interval);
@@ -24,7 +24,7 @@ app.directive('time', function(dateFilter, $interval){
             })
 
             interval = $interval(function(){
-                scope.time = dateFilter(new Date(), 'hh:mm:ss');
+                scope.time = dateFilter(new Date(80), 'hh:mm:ss');
                 console.log('time changed');
             }, 1000)
         }
