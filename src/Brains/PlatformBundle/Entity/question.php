1f2431de1091c025b22334f4c162dd0a94d9320a
brookkk <grini.abdellah@gmail.com>
2017-09-22 13:46:01 +0100

    ui

---
+++ src/Brains/PlatformBundle/Entity/question.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Brains\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * question
+ *
+ * @ORM\Table(name="question")
+ * @ORM\Entity(repositoryClass="Brains\PlatformBundle\Repository\questionRepository")
+ */
+class question
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="question", type="string", length=255)
+     */
+    private $question;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="reponse", type="string", length=255)
+     */
+    private $reponse;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set question
+     *
+     * @param string $question
+     *
+     * @return question
+     */
+    public function setQuestion($question)
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    /**
+     * Get question
+     *
+     * @return string
+     */
+    public function getQuestion()
+    {
+        return $this->question;
+    }
+
+    /**
+     * Set reponse
+     *
+     * @param string $reponse
+     *
+     * @return question
+     */
+    public function setReponse($reponse)
+    {
+        $this->reponse = $reponse;
+
+        return $this;
+    }
+
+    /**
+     * Get reponse
+     *
+     * @return string
+     */
+    public function getReponse()
+    {
+        return $this->reponse;
+    }
+}
+
