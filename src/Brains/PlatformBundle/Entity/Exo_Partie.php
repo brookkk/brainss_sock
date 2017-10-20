4697002df26f1ea80169bd63301f711b76096b8e
brook <grini.abdellah@gmail.com>
2017-10-20 15:02:46 +0100

    exo_partie

---
+++ src/Brains/PlatformBundle/Entity/Exo_Partie.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Brains\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Exo_Partie
+ *
+ * @ORM\Table(name="exo__partie")
+ * @ORM\Entity(repositoryClass="Brains\PlatformBundle\Repository\Exo_PartieRepository")
+ */
+class Exo_Partie
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contenu", type="text")
+     */
+    private $contenu;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set contenu
+     *
+     * @param string $contenu
+     *
+     * @return Exo_Partie
+     */
+    public function setContenu($contenu)
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    /**
+     * Get contenu
+     *
+     * @return string
+     */
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+}
+
