d01f69a7b9045bbb93446454568797902450401f
brook <grini.abdellah@gmail.com>
2017-11-24 15:10:59 +0000

    exo_front

--- front2/js/controllers.js
+++ front2/js/controllers.js
@@ -1,9 +1,8 @@
 
 
 
- 	parking.controller("parkingCtrl", function($scope, parkingService2){
+ 	parking.controller("parkingCtrl", function($scope, parkingService2, $http){
 
-			$scope.appTitle = "[Packt] Parking";
 
 
     		$scope.cars = [];
@@ -21,6 +20,42 @@
           $scope.calculateTicket = function(car){
                 $scope.ticket = parkingService2.calculateTicket(car);
             };
+
+
+
+
+
+
+
+            // EXERCICES
+
+
+            $scope.exos = [];
+
+
+            var retreiveExos = function(){
+
+                 $http.get("http://localhost/brainss/web/app_dev.php/api/exercices")
+                    .success(function(data, status, headers, config){
+                        $sope.exos = data;
+                    })
+                    .error(function(data, status, headers, config){
+                        switch(status){
+                            case 401 : {
+                                $scope.message = "You must be Authenticated!";
+                                break;
+                            }
+                            case 500 : {
+                                $scope.message = "Something went wrong!";
+                                break;
+                            }
+                        }
+                        console.log(data,status);
+                    });
+            };
+
+            retreiveExos();
+
 
 
     	});	
