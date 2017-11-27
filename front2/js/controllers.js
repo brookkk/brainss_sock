dc21d07d64ebbab96e7f57ae1d676439db40a0ae
brook <grini.abdellah@gmail.com>
2017-11-27 15:42:00 +0000

    exo_front

--- front2/js/controllers.js
+++ front2/js/controllers.js
@@ -4,7 +4,12 @@
     		$scope.colors=["white", "black", "blue", "red", "silver"];
     		$scope.appTitle="Exercices";
 
-         // EXERCICES
+
+
+
+
+
+         // retreiveExos : se charge de la récupération des exos du BO (Rest API) et les mettre dans "scope.parties"
 
             $scope.parties = [];
 
@@ -29,48 +34,54 @@
                     });
             };
 
-            retreiveExos();
 
-            console.log($scope.parties);
+            // eval_rep : pour un tableau de réponses [t, t, f, f] (récupéré depuis le view) on donne 2^t comme évaluation ->
+            // utile pour évaluer le cas où y a plus qu'une réponse correcte
 
-$scope.evaluation = 0;
-
-//$scope.parties.reponses = [];
-
-
-
-    $scope.eval_rep = function(reponses){
+            $scope.eval_rep = function(reponses){
             var eval=0;
             for(var i=0; i<reponses.length; i++){
-                //console.log("inside evel " + i + " responses[i] " + responses[i] );
                 if(reponses[i]===true)
                     eval += Math.pow(2,i);
                 }
             return eval;
-        };
+            };
 
 
 
 
+            retreiveExos();
 
-            $scope.evaluateForm = function(){
-            $scope.evaluation = 0;
+
+$scope.evaluation = 0;
+$scope.max = 0;
+
+
+
+
+    
+
+
+
+                // fct qui se charge de l'évaluation des réponses : comparaison des réponses données vs la réponse ->
+                // récupérée depuis le BO ; cette fct est appelée depuis le view
+
+                $scope.evaluateForm = function(){
+                //réinitialiser l'évaluation pour que ça n'icrémente pas à chaque clic
+                $scope.evaluation = 0;
+                $scope.max=0;
                
-
-
-
-
-
+               //boucle sur les parties
                 $scope.parties.forEach(function(part){
- 
+                    //boucle sur les questions
                     part.exo_questions.forEach(function(question){
- 
-                $scope.answers = [question.rep_1, question.rep_2, question.rep_3, question.rep_4];
-                var eval = $scope.eval_rep($scope.answers);
-
-                if( eval == question.valeur)
-                    {
-                $scope.evaluation+= ( question.bareme );}
+                        $scope.answers = [question.rep_1, question.rep_2, question.rep_3, question.rep_4];
+                        var eval = $scope.eval_rep($scope.answers);
+                        if( eval == question.valeur){
+                            // L'évaluation de l'exo est incrémentée par 1* le barême de la question
+                            $scope.evaluation+= ( question.bareme );
+                            }
+                        $scope.max += question.bareme;
                
 
 
@@ -78,13 +89,14 @@ $scope.evaluation = 0;
                 });
 
 
-                console.log ("votre score est  " + $scope.evaluation);
+                console.log ("votre score est  " + $scope.evaluation + " / " + $scope.max);
+                //console.log ("Le max est  " + $scope.max);
                 return $scope.evaluation;
                 
             };
 
 
-
+    $scope.evaluateForm();
 
 
 
