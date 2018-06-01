e70dbc57f2d32b71faee8484fdb2a5bff005c50a
brookkk <grini.abdellah@gmail.com>
2018-06-01 15:42:41 +0000

    chat

--- src/Brains/PlatformBundle/Controller/DefaultController.php
+++ src/Brains/PlatformBundle/Controller/DefaultController.php
@@ -45,82 +45,13 @@ class DefaultController extends Controller
 
     }
 
-/*
 
-       public function uiAction()
+     public function chatAction()
     {
-        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED'))
-        return $this->render('BrainsPlatformBundle:Default:ui.html.twig');
-        else return $this->redirectToRoute('login');
-    }
+         return $this->render('BrainsPlatformBundle:Chat:chat.html.twig');
+     }
 
 
-
-
-
-public function n_uiAction(Request $request)
-  {
-//nouvelle instance de l'entité Année
-    $question= new question();
-
-
-    $form = $this->createForm(questionType::class, $question);
-
-
-//si le formulaire est bien rempli, on l'enregistre dans la BD
-    if($request->isMethod('POST')){
-
-      $form->handleRequest($request);
-
- 
-      
-      if($form->isValid()    
-        ){
-
-        
-
-        
-        $em= $this->getDoctrine()->getManager();
-      $em->persist($question);
-      $em->flush();
-
-      $request->getSession()->getFlashBag()->add('notice', 'question Bien enregistré.');
-
-  
-
-
-
-
-
-          return $this->redirectToRoute('BP_show_exercice');
-    }
-  }
-
-//sinon (ou bien premier landing sur le form), on affiche le formulaire
-  return $this->render('BrainsPlatformBundle:Default:n_ui.html.twig', array(
-   'form'=>$form->createView(),
-   ));
-
-}
-
-
-
-public function n_ui_testAction(Request $request)
-  {
-
-
-
-
-
-//sinon (ou bien premier landing sur le form), on affiche le formulaire
-  return $this->render('BrainsPlatformBundle:Default:n_ui_test.html.twig');
-
-    //    return $this->render('BrainsPlatformBundle:Default:n_ui_test.html.twig');
-
-
-}
-
-*/
 
 
 
